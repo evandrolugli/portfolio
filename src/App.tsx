49fe434eef209db3f1ecdc932504f72edc8a9291
Evandro Lugli <evandro.lugli@gmail.com>
2024-06-19 13:14:35 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+export function App() {
+  return (
+    <>
+        Hello World!
+        <div>
+          <Button>Click me</Button>
+        </div> 
+    </>
+  )
+}

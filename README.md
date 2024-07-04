98910d953d6069b055ca08bcdbec14209f0b5253
Evandro Lugli <evandro.lugli@gmail.com>
2024-07-04 22:44:28 +1000

    update resume

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - shadCN/UI (ui component library)
 
 - phosphor-react (library) - not used
-- react-icons/fa (library for icons) - not used yet
+- react-icons/fa (library for icons)
 
 # Package Manager
 - pnpm
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { IoIosCheckmarkCircle } from 'react-icons/io';
+import { FaCode, FaServer, FaCloud, FaAws } from "react-icons/fa";
+import { FaAward, FaGraduationCap, FaCertificate, FaLanguage, FaTools, FaDatabase } from 'react-icons/fa';
 import { IoIosArrowRoundForward } from 'react-icons/io';
 
 export function Resume() {
@@ -11,7 +12,66 @@ export function Resume() {
       </header>
 
       <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Experience</h2>
+        <h2 className="text-2xl font-bold mb-4">Career Overview</h2>
+        <p className="text-gray-400">
+          Experienced Software Professional graduated with a degree in Information Systems, key skills include a range of back and front end technical languages and intercultural experience. Seeking a role in which I can continue to develop my skills within Australian IT support experience.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Key Skills and Attributes</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex items-center">
+            <FaCode className="text-xl text-blue-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">Programming Languages</h3>
+              <p className="text-gray-400 text-sm">C#, Java, JavaScript, CSS, HTML</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <FaTools className="text-xl text-green-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">Frameworks and Libraries</h3>
+              <p className="text-gray-400 text-sm">Spring Framework, .NET Framework</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <FaServer className="text-xl text-purple-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">Protocols and Data Formats</h3>
+              <p className="text-gray-400 text-sm">SOAP, REST, WSDL, JSON</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <FaDatabase className="text-xl text-red-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">Database</h3>
+              <p className="text-gray-400 text-sm">MS SQL Server, MySQL</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <FaTools className="text-xl text-yellow-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">Version Control and Testing</h3>
+              <p className="text-gray-400 text-sm">Git, Unit Testing</p>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <FaCloud className="text-xl text-indigo-500 mr-3" />
+            <div>
+              <h3 className="text-lg font-semibold">DevOps Tools</h3>
+              <p className="text-gray-400 text-sm">
+                Jenkins (Cloudbees), Openshift, Azure, <span className="flex items-center">
+                  <FaAws className="text-2xl text-yellow-500 mr-2" /> AWS Certified
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">Employment History</h2>
 
         <div className="mb-6">
           <div className="flex items-center mb-2">
@@ -75,9 +135,9 @@ export function Resume() {
           <div className="flex items-center mb-2">
             <h3 className="text-xl font-semibold">Product Owner | Laureate International Universities</h3>
             <div className="ml-4 flex space-x-2">
-              <span className="bg-purple-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Market Analysis</span>
-              <span className="bg-purple-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Portfolio Management</span>
-              <span className="bg-purple-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Stakeholder Communication</span>
+              <span className="bg-green-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Market Analysis</span>
+              <span className="bg-green-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Portfolio Management</span>
+              <span className="bg-green-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Stakeholder Communication</span>
             </div>
           </div>
           <p className="text-gray-400 mb-2">Mar 2013 – Feb 2020 (7 years)</p>
@@ -106,9 +166,10 @@ export function Resume() {
           <div className="flex items-center mb-2">
             <h3 className="text-xl font-semibold">.Net Developer | Woow! Brasil</h3>
             <div className="ml-4 flex space-x-2">
-              <span className="bg-yellow-500 text-white py-1 px-2 rounded-lg text-xs font-medium">.NET Framework</span>
-              <span className="bg-yellow-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Microsoft SQL Server</span>
-              <span className="bg-yellow-500 text-white py-1 px-2 rounded-lg text-xs font-medium">ASP.NET MVC</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">ASP.NET MVC</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">SQL Server</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Javascript</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">C#</span>
             </div>
           </div>
           <p className="text-gray-400 mb-2">Sep 2011 - Sep 2012 (1 year and 1 month)</p>
@@ -131,60 +192,90 @@ export function Resume() {
           </ul>
         </div>
 
+        <hr className="border-gray-300 my-6" />
+
+        <div className="mb-6">
+          <div className="flex items-center mb-2">
+            <h3 className="text-xl font-semibold">Software Developer | Grupo TV1</h3>
+            <div className="ml-4 flex space-x-2">
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">.NET Framework</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">SQL Server</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">Javascript</span>
+              <span className="bg-blue-500 text-white py-1 px-2 rounded-lg text-xs font-medium">C#</span>
+            </div>
+          </div>
+          <p className="text-gray-400 mb-2">Aug 2008 - Apr 2009 (9 months)</p>
+          <p className="text-gray-400 mb-2">
+            An internship where I embraced complex coding challenges, showcasing my problem-solving skills and adapting quickly to new frameworks and technologies.
+          </p>
+          <ul className="list-disc list-inside text-gray-300">
+             <li className="flex items-center mb-1">
+             <IoIosArrowRoundForward className="mr-2" />
+             Maintained projects utilising technologies such as HTML, CSS, JavaScript, and C#.
+            </li>
+            <li className="flex items-center mb-1">
+             <IoIosArrowRoundForward className="mr-2" />
+             Modelling and data manipulation in Microsoft SQL Server database.
+            </li>
+          </ul>
+        </div>
       </section>
 
-      <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">Key Skills and Attributes</h2>
-
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold">Technologies:</h3>
-          <ul className="list-disc list-inside text-gray-400 ml-4">
-            <li>Spring Framework (Java), .NET (C#), Microservices, Protocols (SOAP, REST, WSDL, JSON), MS SQL Server, Git, Unit Test, DevOps, Jenkins (Cloudbees), Openshift, Azure, AWS (Certified), HTML, CSS, JavaScript.</li>
-          </ul>
-        </div>
-
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold">Communication and Intercultural competence:</h3>
-          <ul className="list-disc list-inside text-gray-400 ml-4">
-            <li>Bilingual speaker of Portuguese and English, currently studying Spanish.</li>
-            <li>Background of studying and living across three different countries, engaging with a range of cultures.</li>
-          </ul>
-        </div>
-
-        <div className="mb-6">
-          <h3 className="text-xl font-semibold">Teamwork:</h3>
-          <ul className="list-disc list-inside text-gray-400 ml-4">
-            <li>Collaborated with diverse teams to achieve collective goals.</li>
-          </ul>
-        </div>
-
-      </section>
-
-      <section>
+      <section className="mb-6">
         <h2 className="text-2xl font-bold mb-4">Qualifications and Certificates</h2>
-
         <ul className="list-disc list-inside text-gray-400 ml-4">
-          <li>2023 AWS Certified Cloud Practitioner</li>
-          <li>2023 Diploma of Information Technology</li>
-          <li>2012 Bachelor of Science Degree in Information Systems</li>
-          <li>2011 Microsoft Certified Professional</li>
-          <li>2010 English as a Second Language</li>
-          <li>2007 Data Processing Technician Course</li>
+          <li className="flex items-center mb-3">
+            <FaAward className="text-yellow-500 mr-2" />
+            <span className="font-semibold">2023 AWS Certified Cloud Practitioner</span>
+          </li>
+          <li className="flex items-center mb-3">
+            <FaGraduationCap className="text-blue-500 mr-2" />
+            2023 Diploma of Information Technology
+          </li>
+          <li className="flex items-center mb-3">
+            <FaGraduationCap className="text-blue-500 mr-2" />
+            2012 Bachelor of Science Degree in Information Systems
+          </li>
+          <li className="flex items-center mb-3">
+            <FaCertificate className="text-green-500 mr-2" />
+            2011 Microsoft Certified Professional
+          </li>
+          <li className="flex items-center mb-3">
+            <FaLanguage className="text-purple-500 mr-2" />
+            2010 English as a Second Language
+          </li>
+          <li className="flex items-center mb-3">
+            <FaDatabase className="text-red-500 mr-2" />
+            2007 Data Processing Technician Course
+          </li>
         </ul>
-
       </section>
 
-      <section className="mt-8">
-        <h2 className="text-2xl font-bold mb-4">Volunteering</h2>
 
+      <hr className="border-gray-300 my-6" />
+      
+      <section>
+        <h2 className="text-2xl font-bold mb-4">Volunteering</h2>
         <div className="mb-6">
+        <h3 className="text-xl font-semibold">2024 Volunteering at K8SUG Australia</h3>
+          <ul className="list-disc list-inside text-gray-400 ml-4 mb-3">
+            <li>Assisting with event space setup, guest check-in and photography.</li>
+            <li>Engaged with IT professionals to immerse myself fully in the IT industry.</li>
+          </ul>
           <h3 className="text-xl font-semibold">2022 Volunteering at the Hare Krishna Temple</h3>
-          <ul className="list-disc list-inside text-gray-400 ml-4">
+          <ul className="list-disc list-inside text-gray-400 ml-4 mb-3">
             <li>Served food to guests and assisted in the kitchen to prepare food.</li>
             <li>Engaged with locals to build my English skills and immerse myself into the culture.</li>
           </ul>
         </div>
+      </section>
 
+      <section className="mb-2">
+          <h2 className="text-xl font-semibold">Communication and Intercultural competence</h2>
+          <ul className="list-disc list-inside text-gray-400 ml-4">
+            <li>Bilingual speaker of Portuguese and English, currently studying Spanish.</li>
+            <li>Background of studying and living across three different countries, engaging with a range of cultures.</li>
+          </ul>
       </section>
 
     </div>
